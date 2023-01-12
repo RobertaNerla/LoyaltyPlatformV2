595342bf0374f5bae78cf40f9da3de6455c27483
Staffilon <teghipco@gmail.com>
2023-01-12 17:42:26 +0100

    Added core classes for the project.

---
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/FDTemplatesCatalog.java
@@ -0,0 +1,6 @@
+package it.unicam.cs.ids.loyaltyplatform;
+
+public class FDTemplatesCatalog {
+    FDTemplate templatesList;
+
+}
---
+++ src/test/java/it/unicam/cs/ids/loyaltyplatform/FDTemplatesCatalogTest.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.ids.loyaltyplatform;
+
+public class FDTemplatesCatalogTest {
+}
